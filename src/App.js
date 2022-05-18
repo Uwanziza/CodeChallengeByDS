1337428cb00452432719a925b05e31816bf7cf1f
Uwanziza <uwachanelle@gmail.com>
2022-05-18 17:34:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import  LineChart from './components/Linehart';
+import './App.css';
+
+const App =() =>{
+  return (
+    <div className='app'> 
+    <h2>chart </h2>
+    <div className='wrapper'>
+   < LineChart/>
+      </div>
+  </div>
+
+  );
+}
+export default App; 
---
+++ src/components/Linehart.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Line } from 'react-chartjs-2'
+
+
+const LineChart =()=>{
+    const state={
+        labels:["Jan","Feb","March","April","May","June","July","Augst","Sept ","oct","Nov","Dec"],
+        datasets:[
+            {
+              label:"Mentee rigistration trend",
+              backgroundColor:"gray",
+              borderColor:"white",
+              fill:true,
+              borderWidth:"2",
+              pointBorderColor:"lightbrown",
+              pointBorderWidth:5,
+              pointRadius:8,
+              tension:0.4,
+              data:[0,40,80,160,240],
+            }
+        ]
+    };
+    const options ={
+        scales:{
+            yAxes:[
+                {
+                ticks:{
+                  beginAtZero:true,  
+                },
+                },
+            ],
+        },
+    };
+
+    return(
+        <div>
+           <Line state={state} options={options} />
+           <p>bar chart</p>
+              
+            
+        </div>
+    )
+}
+export default LineChart;
